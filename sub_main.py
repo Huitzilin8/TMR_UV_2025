ddb9764e4e08b9d60a4dce80a12d838e89871e92
José Antonio Villar Caparroso <152246097+Huitzilin8@users.noreply.github.com>
2025-05-08 04:55:38 -0600

    Update sub_main.py

--- sub_main.py
+++ sub_main.py
@@ -1,15 +1,7 @@
 import subprocess
 
-# --- Define the command ---
-# It's best to provide the command and its arguments as a list.
-# For example, to run 'ls -l /tmp'
 command = ["ls", "-l", "/tmp"]
-# Or a simpler command like 'mkdir new_directory'
-# command = ["mkdir", "new_directory"]
-
 try:
-    # --- Execute the command ---
-    # subprocess.call() will execute the command and wait for it to complete.
     # It returns the exit status of the command (0 usually means success).
     print "Executing command: {}".format(" ".join(command))
     return_code = subprocess.call(command)
